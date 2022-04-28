4b0f4ee330b4427323a89633d4ce5444f47da707
EugeneTired <104016719+EugeneTired@users.noreply.github.com>
2022-04-28 11:33:05 +0300

    Add files via upload

---
+++ Project/assets/js/mainpage.js
@@ -0,0 +1,74 @@
+$(window).scroll(function() {
+        if ($(window).scrollTop() > 10) {
+            $('#button').addClass('show');
+        } else {
+            $('#button').removeClass('show');
+        }
+    });
+
+    $('#button').on("click",function(){
+        $(window).scrollTop(0);
+    });
+    $('#inpBut').on("focus",function(){
+      $("#but").toggleClass('show');
+    });
+
+
+    const burgAnimation = () =>{
+    var burger = document.querySelector('.svgburg')
+    var path1 = document.querySelector('.path1')
+    var path2 = document.querySelector('.path2')
+    var mline = document.querySelector('.mline')
+    burger.addEventListener('click',() =>{     
+        path1.classList.toggle('cross');
+        path2.classList.toggle('cross');
+        mline.classList.toggle('hide');
+       }
+     )
+ 
+ }
+burgAnimation();
+
+const toggleButton = document.querySelector('.toggle-button')
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const svgburg = document.getElementsByClassName('svgburg')[0];
+const wrapper = document.querySelector(".wrapper");
+const a = document.querySelector('.toggle-button');
+
+
+
+
+svgburg.addEventListener('click',()=>{
+  if(toggleButton.classList[1] != 'active'){
+      toggleButton.classList.add('active');
+      wrapper.style.opacity = 0.3;
+      toggleButton.style.opacity = 1;
+  } else {
+      toggleButton.classList.remove('active');
+      wrapper.style.opacity = 1;
+      toggleButton.style.opacity = 0.4;
+      toggleButton.animate({ opacity: "0" },400);
+  }
+});
+
+$(document).ready(function(){
+
+      $(".toggle-button").css("height", $(document).height());
+
+      $(".alert").click(function(){
+          $(".toggle-button").fadeIn();
+          return false;
+      });
+
+      $(".close").click(function(){
+          $(".toggle-button").fadeOut();
+          return false;
+      });
+
+  });
+
+  $(window).bind("resize", function(){
+      $(".toggle-button").css("height", $(window).height());
+});
+
+
