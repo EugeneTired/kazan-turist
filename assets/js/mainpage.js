b55017cd5e82e20540b2b21ccded3f5b47263b39
EugeneTired <arbuzkeshaichesnok@mail.ru>
2022-05-02 17:56:46 +0300

    minor adjustments

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -1,36 +1,36 @@
-$(window).scroll(function() {
-        if ($(window).scrollTop() > 10) {
-            $('#button').addClass('show');
-        } else {
-            $('#button').removeClass('show');
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 10) {
+        $('#button').addClass('show');
+    } else {
+        $('#button').removeClass('show');
+    }
+});
+
+$('#button').on("click", function () {
+    $(window).scrollTop(0);
+});
+
+$('#inpBut').on("focus", function () {
+    $("#but").add('show');
+});
+$('#inpBut').on("focusout", function () {
+    $("#but").remove('show');
+});
+
+
+const burgAnimation = () => {
+    burger.addEventListener('click', () => {
+            path1.classList.toggle('cross');
+            path2.classList.toggle('cross');
+            mline.classList.toggle('hide');
         }
-    });
+    )
 
-    $('#button').on("click",function(){
-        $(window).scrollTop(0);
-    });
-
-    $('#inpBut').on("focus",function(){
-      $("#but").add('show');
-    });
-    $('#inpBut').on("focusout",function(){
-      $("#but").remove('show');
-    });
-
-
-    const burgAnimation = () =>{
-    burger.addEventListener('click',() =>{     
-        path1.classList.toggle('cross');
-        path2.classList.toggle('cross');
-        mline.classList.toggle('hide');
-       }
-     )
- 
- }
-    var burger = document.querySelector('.svgburg')
-    var path1 = document.querySelector('.path1')
-    var path2 = document.querySelector('.path2')
-    var mline = document.querySelector('.mline')
+}
+var burger = document.querySelector('.svgburg')
+var path1 = document.querySelector('.path1')
+var path2 = document.querySelector('.path2')
+var mline = document.querySelector('.mline')
 burgAnimation();
 
 const toggleButton = document.querySelector('.toggle-button')
@@ -39,21 +39,19 @@ const wrapper = document.querySelector(".wrapper");
 const a = document.querySelector('.toggle-button');
 
 
-
-
-svgburg.addEventListener('click',()=>{
+svgburg.addEventListener('click', () => {
     wrapper.style.opacity = toggleButton.classList[1] === 'active' ? 1 : 0.2;
     toggleButton.classList.toggle('active');
 });
 
-toggleButton.addEventListener("click",()=>{
-  if(toggleButton.classList[1] !== undefined){
-      toggleButton.classList.remove('active');
-      wrapper.style.opacity = 1;
-      toggleButton.style.opacity = 0.2;
-      toggleButton.animate({ opacity: "0" },400); 
+toggleButton.addEventListener("click", () => {
+    if (toggleButton.classList[1] !== undefined) {
+        toggleButton.classList.remove('active');
+        wrapper.style.opacity = 1;
+        toggleButton.style.opacity = 0.2;
+        toggleButton.animate({opacity: "0"}, 400);
         path1.classList.toggle('cross');
         path2.classList.toggle('cross');
         mline.classList.toggle('hide');
-  }     
+    }
 });
